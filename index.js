89d3309db1f84811925313bda610129817720359
unknown <zhouguoke1984@gmail.com>
2023-12-19 17:11:20 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+let messages = {};
+
+app.get('/in', function (req, res) {
+    for (const key in req.query) {
+        if (Object.hasOwnProperty.call(req.query, key)) {
+            const element = req.query[key];
+            messages[key]=element;
+        }
+    }
+    res.send("ok")
+ })
+
+ app.get('/out/:key', function (req, res) {
+    let key = req.params.key;
+    res.send(messages[key]);
+ })
+
+
+
+
+app.listen(9090);
+
